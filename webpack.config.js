e55e67e6ffdc712a5b00dd900744151cadb26ee8
NULLCT <cockatiel.u10@gmail.com>
2021-06-05 17:12:24 +0900

    change to Nuxt.js

--- webpack.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  entry: __dirname + "/src/js/main.js", //ビルドするファイル
-  output: {
-    path: __dirname + "/dist", //ビルドしたファイルを吐き出す場所
-    filename: "bundle.js", //ビルドした後のファイル名
-  },
-};
