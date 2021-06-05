ae6d4343750dc8e0a36823fc2b281af3ca7123a1
NULLCT <cockatiel.u10@gmail.com>
2021-06-05 18:21:42 +0900

    add hero

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,20 @@
 <template>
-  <div>
-    <p>Hi there!</p>
-  </div>
+  <section class="hero is-fullheight">
+    <div class="hero-body">
+      <div>
+        <p class="title typing-demo">
+          Hi there
+        </p>
+        <p class="subtitle">
+          NULLCT.github.io
+        </p>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
 </script>
+
+<style scoped>
+</style>
