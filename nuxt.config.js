86f280d6850d342831ca7867aeea41f15550501e
NULLCT <cockatiel.u10@gmail.com>
2021-06-06 14:27:10 +0900

    attach to github pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,6 +5,10 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
+  router: {
+    base: '/NULLCT.github.io/'
+  },
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'NULLCT',
